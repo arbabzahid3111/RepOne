902debf0124ed4ca7d1c93a8fb410f617f061207
arbabzahid3111 <125601344+arbabzahid3111@users.noreply.github.com>
2023-02-16 11:19:44 -0800

    Practice

---
+++ Love.py
@@ -0,0 +1,32 @@
+#Arithmetic Operator
+#print(7**2)
+
+#print(20/6)
+#print(20//6)
+
+#Assignment-Operator
+
+#a=5
+#print(5) simple print 5
+#a=a+3
+#print(a) addition of 3 in 5
+#a+=3
+#print(a) same addition in another way
+#a=5
+#a=a-3
+#print(a) decrement method
+#a=20
+#b=10
+#if a>b:
+   # print("a")
+#else:
+   # print("b")
+a=10
+b=10
+#print(a==b)
+print(a!=b)
+
+
+print(b is greaterthan, a)
+
+
